e02a42ce308f558e018614cef58be898f42215d7
junklea <taricerkan@gmail.com>
2020-12-20 20:40:12 +0300

    initial commit + splash screen

---
+++ README.md
@@ -0,0 +1,11 @@
+# meet_up
+
+An app for people to easily arrange meeting with their friends.
+
+- [ ] Onboarding Screen
+- [ ] Log In/Sign Up Screen
+- [ ] Main Screens
+- [ ] Firebase / Google Auth
+- [ ] Simple CRUD
+- [ ] ???
+- [ ] Profit
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import './constants.dart';
+import './screens/splash/splash_screen.dart';
+import 'routes.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Meet Up',
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Muli',
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: kTextColor),
+          bodyText2: TextStyle(color: kTextColor),
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: SplashScreen.routeName,
+      routes: routes,
+    );
+  }
+}
